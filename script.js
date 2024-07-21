32b4c18159d69412179de53d1dec4b0777d99808
Sumeet Ravindra Devrukhkar <Sumeet.17758@sakec.ac.in>
2024-07-21 15:16:47 +0530

    Modal Button

---
+++ script.js
@@ -0,0 +1,9 @@
+let popupmodal = document.querySelector('.popupmodal');
+
+let openModal = () => {
+    popupmodal.classList.add('popup_open');
+}
+
+let closeModal = () =>{
+    popupmodal.classList.remove('popup_open');
+}
